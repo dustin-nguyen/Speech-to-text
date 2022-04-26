6b6dc373713d01eb3f8e41f7e650a9d3965de651
Duc Nguyen <ducnrg@gmail.com>
2022-04-25 20:47:28 -0400

    change local host

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 const router = express.Router({ mergeParams: true });
 const path = require('path');
 
